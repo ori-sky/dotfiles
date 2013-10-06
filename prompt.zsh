af120f38b191d0aa6fc09de5a69fc52d6281f822
Shockk <shokku.ra@gmail.com>
2013-10-06 23:44:04 +0000

    Added zsh prompt config.

---
+++ prompt.zsh
@@ -0,0 +1,6 @@
+PROMPT='%{$fg_bold[green]%}➜ %{$fg_bold[yellow]%}%2~%{$reset_color%}$(git_prompt_info)%{$reset_color%} %% '
+
+ZSH_THEME_GIT_PROMPT_PREFIX=" (%{$fg_bold[green]%}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%})"
+ZSH_THEME_GIT_PROMPT_DIRTY=" %{$fg_bold[red]%}✗"
+ZSH_THEME_GIT_PROMPT_CLEAN=""
