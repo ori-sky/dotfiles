5c8489431adc49900e1b8aa47bef6d02626d8e5c
Shockk <shokku.ra@gmail.com>
2016-02-15 23:46:09 +0000

    Update and import new dotfiles

--- util/battery_percent.sh
+++ util/battery_percent.sh
@@ -3,11 +3,11 @@ BAT_NOW=`cat /sys/class/power_supply/BAT0/charge_now`
 BAT_FULL=`cat /sys/class/power_supply/BAT0/charge_full`
 BAT_FULL_DESIGN=`cat /sys/class/power_supply/BAT0/charge_full_design`
 
-PROG="$BAT_NOW / $BAT_FULL_DESIGN * 100;"
+PROG="$BAT_NOW / $BAT_FULL * 100;"
 PER_NOW=`echo "$PROG" | bc -l`
 
 PROG="$BAT_FULL / $BAT_FULL_DESIGN * 100;"
 PER_FULL=`echo "$PROG" | bc -l`
 
 echo "$PER_NOW" | awk '{printf "%.2f%% of ", $0}'
-echo "$PER_FULL" | awk '{printf "%.0f%%", $0}'
+echo "$PER_FULL" | awk '{printf "%.2f%%", $0}'
