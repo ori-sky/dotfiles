f2d47f361f347baa2b0969e2d77bac1fc9ac851a
Shockk <shockk@orange.local>
2016-05-01 14:19:09 +0000

    xmonad: support multiple monitors

--- xmonad.hs
+++ xmonad.hs
@@ -1,27 +1,38 @@
 import System.IO (hPutStrLn)
 import XMonad
-import XMonad.Util.Run (spawnPipe)
-import XMonad.Hooks.DynamicLog
 import XMonad.Layout.Gaps
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.EZConfig
+import qualified XMonad.StackSet as W
 
 main = do
     xmproc <- spawnPipe "xmobar"
     spawnPipe "compton -c"
     spawnPipe "xscreensaver"
-    xmonad defaultConfig
+    xmonad $ defaultConfig
         { terminal      = "urxvtcd"
-        , workspaces    = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        , logHook       = logHook' xmproc
-        , layoutHook    = layoutHook'
+        , workspaces    = myWorkspaces
+        , logHook       = myLogger xmproc
+        , layoutHook    = myLayout
         , modMask       = mod5Mask
-        }
+        } `additionalKeysP` myKeys
 
-logHook' xmproc = dynamicLogWithPP xmobarPP
+myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+myLayout = gaps [(U, 16)] $ Mirror (Tall 1 (3/100) (1/2)) ||| Full
+
+myKeys = [(otherModMasks ++ "M-" ++ [key], action tag)
+          | (tag, key) <- zip myWorkspaces "123456789"
+          , (otherModMasks, action) <- [ ("", windows . W.view)
+                                       , ("S-", windows . W.shift)
+                                       ]
+         ]
+
+myLogger xmproc = dynamicLogWithPP xmobarPP
     { ppOutput  = hPutStrLn xmproc
     , ppCurrent = xmobarColor "#eee8d5" "" . wrap "[" "]"
     , ppTitle   = xmobarColor "#586e75" "" . shorten 60
     , ppSep     = " <fc=#ffccaa>:</fc> "
     , ppOrder   = \(ws:_:xs) -> ws : xs
     }
-
-layoutHook' = gaps [(U, 16)] $ Mirror (Tall 1 (3/100) (1/2)) ||| Full
